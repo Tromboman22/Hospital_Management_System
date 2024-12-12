553a276f7eabf0cf8b34deb1a8ad67feb92ea575
Tromboman22 <109414093+Tromboman22@users.noreply.github.com>
2024-12-12 17:59:39 -0500

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -1,2 +1,42 @@
 #pragma once
+#include <string>
 
+using namespace std;
+
+class Patient {
+    private:
+        string firstName, lastName, 
+        diagnostic, blood, // blood type
+        DOB, admission, discharge; //all dates
+
+        long int ID, doctor; //doctor ID number
+ 
+        public:
+            Patient();
+
+            //names
+            string getFirstName(); 
+            void setFirstName(string name);
+            string getLastName(); 
+            void setLastName(string name);
+
+            //medical info
+            string getDiagnostic();
+            void setDiagnostic(string diag);
+            string getBlood();
+            void setBlood(string type);
+
+            //dates
+            string getDOB();
+            void setDOB(string date);
+            string getAdmission();
+            void setAdmission(string date);
+            string getDischarge();
+            void setDischarge(string date);
+
+            //ID's
+            long int getID();
+            void setID(long int num);
+            long int getDoctor();
+            void setDoctor(long int num);
+};
