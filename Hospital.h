701ea10447af5b8070a1a1caf47fa90bdfaf21d1
Tromboman22 <109414093+Tromboman22@users.noreply.github.com>
2024-12-12 20:35:46 -0500

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -20,7 +20,7 @@ class Hospital
         void Count_Critical_Patients();
         void Doctors_By_Specialty(string specialty);
         void Show_Patient_By_ID(long int id);
-        void Show_DOctor_By_ID(long int id);
+        void Show_Doctor_By_ID(long int id);
         void Show_Assigned_Doctor(long int id);
         void Show_Assigned_Patients(long int id);
 };
