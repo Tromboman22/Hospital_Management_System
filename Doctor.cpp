41399a1d1b5ebdb9b83ea7ea9946714860faa492
tudormafteuta <tudor.mafteuta@yahoo.ca>
2024-12-12 19:14:24 -0500

    Add files via upload

---
+++ Doctor.cpp
@@ -0,0 +1,81 @@
+#include "Doctor.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+Doctor::Doctor(){
+    firstName = "";
+    lastName = "";
+    specialty = "";
+    id = 0;
+    yearsExp = 0;
+    salary = 0;
+    performanceBonus = 0;
+}
+
+Doctor::Doctor(const string& fName, const string& lName, const string& inpSpeciality, const int& inpId, const int& years, const double& inpSalary, const double& perfBonus){
+    firstName = fName;
+    lastName = lName;
+    specialty = inpSpeciality;
+    id = inpId;
+    yearsExp = years;
+    salary = inpSalary;
+    performanceBonus = perfBonus;
+}
+
+void Doctor::setFirstName(string name){
+    firstName = name;
+}
+
+string Doctor::getFirstName(){
+    return firstName;
+}
+    
+
+void Doctor::setLastName(string name){
+    lastName = name;
+}
+string Doctor::getLastName(){
+    return lastName;
+}
+
+void Doctor::setSpecialty(string name){
+    specialty = name;
+}
+string Doctor::getSpecialty(){
+    return specialty;
+}
+
+void Doctor::setId(int inpId){
+    id = inpId;
+}
+int Doctor::getId(){
+    return id;
+}
+void Doctor::setYearsExp(int years){
+    yearsExp = years;
+}
+int Doctor::getYearsExp(){
+    return yearsExp;
+}
+void Doctor::setSalary(int inpSalary){
+    salary = inpSalary;
+}
+double Doctor::getSalary(){
+    return salary;
+}
+
+void Doctor::setPerformanceBonus(double bonus){
+    performanceBonus = bonus;
+}
+double Doctor::getPerformanceBonus(){
+    return performanceBonus;
+}
+
+double Doctor::Calculate_Salary(){
+    return salary + salary*performanceBonus;
+}
+void Doctor::Print_Doctor_Info(){
+    cout << firstName << " " << lastName << " " << id << " " << specialty << " " << yearsExp << " " << salary << " " << performanceBonus;
+}
---
+++ Doctor.h
@@ -0,0 +1,49 @@
+#ifndef DOCTORS.H
+#define DOCTORS.H
+
+#include <string>
+using namespace std;
+
+class Doctor {
+
+private:
+    string firstName, lastName, specialty;
+    int id, yearsExp;
+    double salary, performanceBonus;
+    
+
+public:
+
+    Doctor();
+    Doctor(const string& fName, const string& lName, const string& inpSpeciality, const int& inpId, const int& years, const double& inpSalary, const double& perfBonus);
+
+    void setFirstName(string name);
+    string getFirstName();
+
+    void setLastName(string name);
+    string getLastName();
+
+    void setSpecialty(string name);
+    string getSpecialty();
+
+    void setId(int inpId);
+    int getId();
+
+    void setYearsExp(int years);
+    int getYearsExp();
+
+    void setSalary(int inpSalary);
+    double getSalary();
+
+    void setPerformanceBonus(double bonus);
+    double getPerformanceBonus();
+
+    double Calculate_Salary();
+
+    void Print_Doctor_Info();
+
+
+
+
+};
+#endif
